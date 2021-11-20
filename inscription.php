cec19e390bbd5b06c4a00cbbd0624dd477dad3f2
omar-diane <omar.diane@laplateforme.io>
2021-11-20 20:20:57 +0100

    Fichier modifié

--- inscription.php
+++ inscription.php
@@ -1,46 +1,28 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Inscription</title>
-</head>
-<body>
-    <?php
-
-    if (isset($_REQUEST ['nom'], $_REQUEST ['prenom'], $_REQUEST ['password'])) {
-        $name = stripslashes ($_REQUEST ['name']);
-        $name = mysqli_real_escape_string ($conn,$name);
-
-        $prenom = stripslashes ($_REQUEST ['prenom']);
-        $prenom = mysqli_real_escape_string ($conn, $prenom);
-
-        $password = stripslashes ($_REQUEST ['password']);
-        $password = mysqli_real_escape_string ($conn, $password);
-
-        $query = "INSERT into `moduleconnexion` (name, prenom, password)
-              VALUES ('$name', '$prenom', '".hash('sha256', $password)."')";
-
-        $res = mysqli_query ($conn, $query);
-        if ($res){
-            echo "<div class='success'>
-            <h3> Vous êtes inscrit avec succés </h3>
-            <p> Cliquer ici pour vous <a href='connexion.php'>connecter</a> </p>
-            </div>";
-        }      
-    } else {
-    ?>
-    <form class="box" action="" method="post">
-    <h1 class="box-title">S'inscrire</h1>
-  <input type="text" class="box-input" name="name" placeholder="Nom" required />
-  <input type="text" class="box-input" name="prenom" placeholder="Prenom" required />
-    <input type="password" class="box-input" name="password" placeholder="Mot de passe" required />
-    <input type="submit" name="submit" value="S'inscrire" class="box-button" />
-    <p class="box-register">Déjà inscrit ? <a href="connexion.php">Connectez-vous ici</a></p>
+<form action="" method="post">
+    <label>Login: <input type="text" name="login"/></label><br/>
+    <label>Nom: <input type="text" name="nom"/></label><br/>
+    <label>Prenom: <input type="text" name="prenom"/></label><br/>
+    <label>Mot de passe: <input type="password" name="passe"/></label><br/>
+    <label>Confirmation du mot de passe: <input type="password" name="passe2"/></label><br/>
+    <input type="submit" value="M'inscrire"/>
 </form>
-<?php } ?>
-</body>
-</html>
-</body>
-</html>
+
+<?php
+
+if(empty($_POST['login'])){
+    mysqli_connect("localhost", "root","", "moduleconnexion");
+    $passe = mysqli_real_escape_string(htmlspecialchars($_POST['passe']));
+    $passe2 = mysqli_real_escape_string(htmlspecialchars($_POST['passe2']));
+    if($passe ==$passe2){
+        $login = mysqli_real_escape_string(htmlspecialchars($_POST['login']));
+        $name = mysqli_real_escape_string(htmlspecialchars($_POST['nom']));
+        $prenom = mysqli_real_escape_string(htmlspecialchars($_POST['prenom']));
+
+        $passe = sha1($passe);
+
+        mysqli_query("INSERT INTO utilisateurs VALUES ('$login', '$name', '$prenom', '$passe')");
+    } else{
+        echo 'Les mots de passe ne correspondent pas';
+    }
+}
+?>
