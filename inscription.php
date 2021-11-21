f248fba2d7ea763bea10ebec4772e147f2a818c0
omar-diane <omar.diane@laplateforme.io>
2021-11-21 10:57:48 +0100

    Modification page inscription

--- inscription.php
+++ inscription.php
@@ -1,28 +1,56 @@
-<form action="" method="post">
-    <label>Login: <input type="text" name="login"/></label><br/>
-    <label>Nom: <input type="text" name="nom"/></label><br/>
-    <label>Prenom: <input type="text" name="prenom"/></label><br/>
-    <label>Mot de passe: <input type="password" name="passe"/></label><br/>
-    <label>Confirmation du mot de passe: <input type="password" name="passe2"/></label><br/>
-    <input type="submit" value="M'inscrire"/>
-</form>
-
 <?php
+$db_server = 'localhost';
+$$db_username = 'root';
+$db_password = '';
+$db = 'moduleconnexion';
 
-if(empty($_POST['login'])){
-    mysqli_connect("localhost", "root","", "moduleconnexion");
-    $passe = mysqli_real_escape_string(htmlspecialchars($_POST['passe']));
-    $passe2 = mysqli_real_escape_string(htmlspecialchars($_POST['passe2']));
-    if($passe ==$passe2){
-        $login = mysqli_real_escape_string(htmlspecialchars($_POST['login']));
-        $name = mysqli_real_escape_string(htmlspecialchars($_POST['nom']));
-        $prenom = mysqli_real_escape_string(htmlspecialchars($_POST['prenom']));
+$conn = mysqli_connect($db_server, $db_username, $db_password, $db);
+if (isset($_POST['login']) ($_POST['login']) != '')
+        (isset($_POST['nom']) ($_POST['nom']) != '')
+	 	(isset($_POST['prenom']) ($_POST['prenom']) != '')
+	 	(isset($_POST['password']) ($_POST['password']) != '')){
 
-        $passe = sha1($passe);
+			$login = $_POST['login'];
+			$name = $_POST['nom'];
+			$prenom = $_POST['prenom'];
+			$password = $_POST['password'];
 
-        mysqli_query("INSERT INTO utilisateurs VALUES ('$login', '$name', '$prenom', '$passe')");
-    } else{
-        echo 'Les mots de passe ne correspondent pas';
-    }
+			$query= " INSERT INTO utilisateurs( login, prenom, nom, password) VALUES ('$login','$name','$prenom','$password') ";
+
+			$req = mysqli_query($conn,$quest);
+
+			if(isset($_POST['submit'])){
+			
+			header( "Location: connexion.php" );
+
+			}
+
+	} else {
+
+		echo 'Tous les champs ne sont pas renseignés';
+	}
+
+   
 }
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="post">
+        <label for=""> Login <input type="text" name="login"></label>
+        <label for=""> Nom <input type="text" name="name"></label>
+        <label for=""> Prénom <input type="text" name="prenom"></label>
+        <label for=""> Mot de passe <input type="password" name="password"></label>
+        <label for=""> Confirmation du mot de passe <input type="password" name="confirmpassword"></label>
+    </form>
+
+    <a href="index.php"></a>
+</body>
+</html>
