3c1f13a5ba900f8c5a84752c6d62d766386c1ae9
omar-diane <omar.diane@laplateforme.io>
2021-11-22 20:45:21 +0100

    connexion correction

--- connexion.php
+++ connexion.php
@@ -29,19 +29,84 @@ $db_server = 'localhost';
 			
 			$conn = mysqli_connect($db_server, $db_user, $db_password, $db);
 
-            if(isset($_POST['login']) AND ($_POST['login']) !=''){
-                foreach($users as $users){
-                    if(
-                        $users['login']==$_POST ['login'] AND
-                        $users['password']==$_POST ['password'] AND
-                    ) {
-                        $loggedUsers = [
-                            'login' => $users['login']
-                        ];
-                    } else {
-                        $erroMessage = sprintf("Les informations envoyer ne permettent pas de vous identifier")
-                    }
-                }
-            }
+            $query = "SELECT login FROM utilisateurs "; 
+
+			$req = mysqli_query($conn,$query);
+
+			$res = mysqli_fetch_all($req, MYSQLI_ASSOC);
+
+            $admin=0;
+            $usercheck=0;
+
+            if (	(isset($_POST["login"])) and $_POST['login'] != ''){				// check if empty and exists
+                if (	(isset($_POST["password"])) and $_POST['password'] != '') {
+
+                    foreach($res as $k => $v){
+
+                        if( $_POST['login'] === $v['login'] and	$_POST['login'] !== 'admin' ){
+
+                            $usercheck++;
+
+                        } elseif ($_POST['login'] === 'admin') {	
+                            
+                            $admin++;
+
+                        }
+
+                        if ($_POST['password'] === $v['password'] and $_POST['password'] !== 'admin' ){
+
+                            $usercheck++;
+
+                                if ($usercheck === 2 ){
+
+                                    $_SESSION['connected']=$_POST['login'];
+                                    $_SESSION['login']=$_POST['login'];
+
+
+                                    header( 'Location: profil.php');
+                                }
+
+                        }	elseif ($_POST['login'] === 'admin') {
+
+                            $admin++;
+
+                            if ($admin === 2){
+
+                                $_SESSION['adminconnected']=$_POST['login'];
+
+                                    header( 'Location: admin.php');
+                            }
+                        }
+                    }	 
+
+                }	else {	echo 'please insert your password'; }
+            }	else { echo 'please insert your login name';}
+
+
+                        if(	(isset($_POST['login']) === $v))	{
+                        if(	(isset($_POST['password']) === $v))	{
+
+                        	$_SESSION['connected'][]=$_POST['submit']; 
+
+                            $_SESSION['login'][]=$_POST['login']; 
+
+                            header('Location: profil.php');
+                                    
+                            }else {
+                                echo '<a href="inscription.php">Subscribe to Log In</a>';
+                                }
+                        }
+
+                            if ($_POST['login'] === 'admin' ) {
+                            $admin++;
+                         
+                            } 
+                            if ($_POST['password'] === 'admin' )  {	
+                                        $admin++; 
+                                        if( $admin === 2 ){
+                                            header( 'Location: admin.php');
+                                        }
+                                    }
+
 
 ?>
