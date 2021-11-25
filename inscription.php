3c4f14c7c4fb19dcb58a847ed8c093eb9f243693
omar-diane <omar.diane@laplateforme.io>
2021-11-25 19:47:35 +0100

    Modif php page inscription

--- inscription.php
+++ inscription.php
@@ -19,6 +19,21 @@
 <?php
 
 require('config.php');
+<?php
+// Informations d'identification
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'moduleconnexion');
+ 
+// Connexion à la base de données MySQL 
+$conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+ 
+// Vérifier la connexion
+if($conn === false){
+    die("ERREUR : Impossible de se connecter. " . mysqli_connect_error());
+}
+?>
 
 if (isset($_REQUEST['login'], $_REQUEST['name'], $_REQUEST['prenom'], $_REQUEST['password'])){
     // récupérer le login 
