39f9a9efdbe49f2b6d63870bad94f2f6f34eaeb1
omar-diane <omar.diane@laplateforme.io>
2022-01-04 11:11:55 +0100

    page header.php ok. Il manque plus que la barre de nav de la page d'accueil change lorsque l'utilisateur est connecté.

--- header.php
+++ header.php
@@ -1,23 +1,27 @@
 <?php 
 
+$utilcheck=0;
+$admin=0;
 
+if($utilcheck === 2){
+   $_SESSION['connected']=$res[2];
+} elseif ($admin === 2){
+   $_SESSION['adconnected']= $_POST['login'];
+}
 
-   if($_SESSION["adconnected"]=='admin'){
-      echo "<a href='admin.php'> Page admin </a>";
-   } elseif (isset($_SESSION["connected"])){
-      echo "<a href='profil.php'> Profil </a>";
+   if(isset($_SESSION["adconnected"])){
+      echo "<a href='admin.php'> Admin </a>";
+      echo "<a href='logout.php'> Déconnexion </a>";
+   } elseif (isset($_SESSION["connected"])) {
+      echo "<a href='profil.php'> Mon profil </a>";
+      echo "<a href='logout.php'> Déconnexion </a>";
    } else {
-      echo "<a href='inscription.php'> Inscription </a>";
-   }
-
-   if(isset($_SESSION["connected"])){
-   echo "<a href='logout.php'> Déconnexion </a>";
-   } else {
-   echo "<a href='connexion.php'> Connexion </a>";
+      echo"<a href='connexion.php'> Connexion </a>";
    }
 
    if(isset($_POST['logout'])){
    session_destroy();
-   header("Location:index.php");
+   header("Location:connexion.php");
    }
+
 ?>
